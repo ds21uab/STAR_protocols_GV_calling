b492ff8102e1dceafb6588bc0ed97c12d35fa7c0
Divya Sahu <92824345+ds21uab@users.noreply.github.com>
2021-12-06 13:03:50 -0600

    Add files via upload

---
+++ scripts/remove_header_VCF.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Copywrite Divya Sahu, 2021
+
+date +"%d %B %Y %H:%M:%S"
+
+## what this script will do: remove header section from the VCF file
+## where to run this script: run this script where indexed_pass_filtered_variants are located
+
+# processing each VCF file using for loop
+for myfile in *.vcf
+do
+	echo "processing" $myfile
+	samplename="${myfile%.*}.no_header.vcf"
+	awk '! /\##/' $myfile > $samplename
+done
+
+date +"%d %B %Y %H:%M:%S"
+
